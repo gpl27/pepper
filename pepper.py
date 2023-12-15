9b1c24de540330d2e0b1f079efe52e506b647db7
Gustavo Lacroix <gustavolacroix@gmail.com>
2023-12-14 21:10:41 -0300

    Further developed

--- pepper.py
+++ pepper.py
@@ -1,6 +1,16 @@
 # Pepper's Private Band
 # 
+from mido import MidiFile
+import pygame
 
+class Music():
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = MidiFile(filename)
+        self.length = 0
+
+    def calc_length(self):
+        pass
 
 class Composer():
     """
@@ -21,7 +31,6 @@ class Composer():
         self.rules = rules
 
     def compose(self):
-        
         pass
 
 
@@ -33,14 +42,29 @@ class Recorder():
     ou em uma gravação de audio, ou em um playback em tempo
     real
     """
-    def __init__(self, MIDIFile):
-        self.MIDIFile = MIDIFile
-
-    def setMIDIFile(self, MIDIFILE):
-        self.MIDIFile = MIDIFILE
+    def __init__(self, music):
+        self.music = music
+        freq = 44100    # audio CD quality
+        bitsize = -16   # unsigned 16 bit
+        channels = 2    # 1 is mono, 2 is stereo
+        buffer = 1024    # number of samples
+        pygame.mixer.init(freq, bitsize, channels, buffer)
+        pygame.mixer.music.set_volume(0.8) # optional volume 0 to 1.0
 
     def playback(self):
-        pass
+        clock = pygame.time.Clock()
+        pygame.mixer.music.load(self.music.filename)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            # check if playback has finished
+            clock.tick(30)
+        pygame.mixer.music.unload()
+        return
 
     def record(self):
         pass
+
+
+music = Music("test.mid")
+recorder = Recorder(music)
+recorder.playback()
