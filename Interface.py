056f86afb5ba1b2cd3a64d6e153cece7c2411c07
Eduardo Stuani <eduardostuani.cs@gmail.com>
2024-01-24 22:24:07 -0300

    Added error messages and refactor

--- Interface.py
+++ Interface.py
@@ -6,11 +6,20 @@ class Interface:
     Contém os métodos para ler o texto de entrada do usuário.
     """
 
+    # Constants
+    MIN_BPM_VALUE = 20
+    MAX_BPM_VALUE = 200
+    MENU_WIDTH = 500
+    MENU_HEIGHT = 400
+    WINDOW_WIDTH = 720
+    WINDOW_HEIGHT = 720
+    WINDOW_WRAP = 20
+
     # Initializes Dear PyGui context and setup interface (public)
     def __init__(self):
         dpg.create_context()
         self._setup_interface()
-        dpg.create_viewport(title='Title', width=720, height=720)
+        dpg.create_viewport(title='Title', width=self.WINDOW_WIDTH, height=self.WINDOW_HEIGHT)
         dpg.setup_dearpygui()
         dpg.show_viewport()
         dpg.set_primary_window("main_window", True)
@@ -26,25 +35,31 @@ class Interface:
         pass
 
     def _menu_help(self):
-        width = 500
-        height = 400
-        with dpg.window(label="Help Window", width=width, height=height, show=True, no_collapse=True, no_resize=False):
-            with dpg.group(width=width):
-                dpg.add_text("The main goal of the software is to generate music from text.", wrap=width-20)
+        with dpg.window(label="Help Window", width=self.MENU_WIDTH, height=self.MENU_HEIGHT, show=True, no_collapse=True, no_resize=False):
+            with dpg.group(width=self.MENU_WIDTH):
+                dpg.add_text("The main goal of the software is to generate music from text.", wrap=self.MENU_WIDTH-self.WINDOW_WRAP)
                 dpg.add_separator()
-                dpg.add_text("The software receives as input an unstructured text (like a short story or newspaper page) and generates a set of notes corresponding to the text according to some parameters (like timbre, rhythm, BPM). The parameters are defined via a mapping of text to musical information.", indent= 1, wrap=width-20)
+                dpg.add_text("The software receives as input an unstructured text (like a short story or newspaper page) and generates a set of notes corresponding to the text according to some parameters (like timbre, rhythm, BPM). The parameters are defined via a mapping of text to musical information.", indent= 1, wrap=self.MENU_WIDTH-self.WINDOW_WRAP)
+    
+    def _btn_generate(self, sender, app_data):
+        # clear error messages
+        for input_error_tag in ["__text_input_error", "__bpm_input_error", "__filename_input_error"]:
+            dpg.configure_item(item=input_error_tag, show=False)
 
-    # Buttons Functions (private)
-    def _btn_generate(self):
         values = dpg.get_values(["__text_input", "__bpm_input", "__filename_input"])
-        if (values[0] and values[1] and values[2]):
-            # TODO: add loading indicator, set UserInput
-            print("penis certo")
-            pass
-        else:
-            # TODO: add popup not valid
-            print("penis errado")
-            pass
+        
+        if not values[0]:
+            self._show_error("__text_input_error", "Text input is required")
+        
+        if not (self.MIN_BPM_VALUE <= int(values[1]) <= self.MAX_BPM_VALUE):
+            self._show_error("__bpm_input_error", f'BPM value must be between {self.MIN_BPM_VALUE} and {self.MAX_BPM_VALUE}')
+        if not values[2]:
+            self._show_error("__filename_input_error", "Filename is required and can't contain an extension in the name")
+        
+        # TODO: add others
+
+    def _show_error(self, tag, text):
+        dpg.configure_item(item=tag, default_value=text, show=True, color=(255, 0, 0, 255))
 
     def _btn_save(self):
         # TODO: check if was generated
@@ -58,12 +73,24 @@ class Interface:
                     dpg.add_menu_item(label="Import File", callback=self._menu_import)
                 dpg.add_menu_item(label="Help", callback=self._menu_help)
 
+            # Input text
             dpg.add_text("Your Text")
             dpg.add_input_text(tag="__text_input", multiline=True)
+            dpg.add_text(tag="__text_input_error", show=False)
+            
+            # Input BPM
             dpg.add_text("BPM")
-            dpg.add_input_int(tag="__bpm_input", min_value=0, max_value=200, min_clamped=True, max_clamped=True)
+            dpg.add_input_int(tag="__bpm_input", min_value=self.MIN_BPM_VALUE, max_value=self.MAX_BPM_VALUE, min_clamped=True, max_clamped=True)
+            dpg.add_text(tag="__bpm_input_error", show=False)
+            
+            # Input Filename
             dpg.add_text("Filename")
             dpg.add_input_text(tag="__filename_input")
+            dpg.add_text(tag="__filename_input_error", show=False)
+            
             dpg.add_button(label="Generate", callback=self._btn_generate)
+            
             dpg.add_separator()
+            
+            # TODO: add song interface (pygame) after generation
             dpg.add_button(label="Save", callback=self._btn_save)
